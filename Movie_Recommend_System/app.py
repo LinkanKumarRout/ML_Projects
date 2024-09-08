5ac8581476d476a9d74a46ee5200edc8898dd519
Linkan Kumar Rout <linkankumarrout@gmail.com>
2024-09-08 22:50:20 +0530

    Initial commit with large files using Git LFS

---
+++ Movie_Recommend_System/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+st.title("Movie Recommendation System")
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]]['title'])  
+
+    return recommended_movies
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movies = pd.DataFrame(movies_dict)
+
+selected_movie_name = st.selectbox(
+    "Choose a movie....",
+    movies['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    st.header("Recommended Movies...")
+    for i in recommendations:
+        st.write(i)  # Display the recommended movies
+
